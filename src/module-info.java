1bb55a64a07a5e0533e82fe8ee86203e99a04ded
MathGuiMach <mathguimach@gmail.com>
2020-11-07 19:06:25 -0300

    MainView Setup

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module workshopJavafxJdbc {
 	requires javafx.controls;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
-	opens gui to javafx.graphics, javafx.fxml;
+
 }
